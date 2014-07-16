74494a0d2e8abde0fa70c17c90162bb7f534d6a4
Moss Amer <mookman74@gmail.com>
2014-07-16 17:23:19 -0400

    Fixed port stored as string error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Merl::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.google.com",
-    port: "587",
+    port: 587,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
